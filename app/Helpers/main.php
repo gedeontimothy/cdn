b2ae19b45b6f5212d03fd3fa6e86932ebcbabe93
gedeontimothy <gedeonbateko3@gmail.com>
2024-09-30 06:14:41 +0100

    [UPDATE] Helpers - Added helper functions `isIpV4Address`, `isUrl` and `isDomain`

--- app/Helpers/main.php
+++ app/Helpers/main.php
@@ -32,3 +32,45 @@ if(!function_exists('checkOS')){
 		return stripos(strtolower(PHP_OS_FAMILY), strtolower($os)) !== false; 
 	}
 }
+
+if(!function_exists('isIpV4Address')){
+	/**
+	 * Checks if a given string is a valid IP address.
+	 *
+	 * @param string $ip
+	 * 
+	 * @return bool
+	 * 
+	 */
+	function isIpV4Address(string $ip) : bool{
+		return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false;
+	}
+}
+
+if(!function_exists('isUrl')){
+	/**
+	 * Checks if a given string is a valid URL.
+	 *
+	 * @param string $url
+	 * 
+	 * @return bool
+	 * 
+	 */
+	function isUrl(string $url) : bool{
+		return filter_var($url, FILTER_VALIDATE_URL) !== false;
+	}
+}
+
+if(!function_exists('isDomain')){
+	/**
+	 * Checks if a given string is a valid Domain.
+	 *
+	 * @param string $domain
+	 * 
+	 * @return bool
+	 * 
+	 */
+	function isDomain(string $domain) : bool{
+		return filter_var($domain, FILTER_VALIDATE_DOMAIN) !== false;
+	}
+}
