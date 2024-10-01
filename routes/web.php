585fd7da8ebf2b75d076935dbdee73b931bc445e
gedeontimothy <gedeonbateko3@gmail.com>
2024-10-01 10:53:54 +0100

    [UPDATE] Routes - Added and updated some routes

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,12 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
-Route::get('/link/{key}', [ReadLinkController::class, 'show'])->name('show.link.file')->where('key', '.*');
+Route::get('/link/{key}', [ReadLinkController::class, 'show'])->name('show.link.file')->where('key', '.*')->middleware('update.link.query');
 
 Route::get('/css/{original_name}', [CssController::class, 'show'])->name('show.css')->where('original_name', '.*');
 Route::get('/js/{original_name}', [JsController::class, 'show'])->name('show.js')->where('original_name', '.*');
-Route::get('/icon/{original_name}', [SvgController::class, 'show'])->name('show.svg')->where('original_name', '.*');
+Route::get('/icon/svg-color/{p}', fn($p) => to_route('show.svg', ['original_name' => $p, 'category' => 'colored']))->where('p', '.*');
+Route::get('/icon/svg/{original_name}', [SvgController::class, 'show'])->name('show.svg')->where('original_name', '.*');
 Route::get('/random/image', [ImageController::class, 'showRandom'])->name('show.random.image')->where('index', '.*');
 Route::get('/image/{quality}/{index}', [ImageController::class, 'showQuality'])->name('show.quality.image')->where(['quality' => '\d+', 'index' => '.*']);
 Route::get('/image/{index}', [ImageController::class, 'show'])->name('show.image')->where('index', '.*');
